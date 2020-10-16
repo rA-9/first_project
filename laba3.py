a97dfc7c21f642f5e5f0c321792c0cd70b856c7b
Zhirova <42269523+Zhirova@users.noreply.github.com>
2020-10-16 21:43:20 +0700

    Update laba3.py
    запись 2 способ

--- laba3.py
+++ laba3.py
@@ -26,3 +26,15 @@ with myFile:
     writer.writerows(myData)
      
 print("Writing complete")
+
+import csv
+
+with open('C:\\Users\\Анна\\Documents\\python\\exp.csv', 'w') as csvfile:
+    fieldnames = ['Last_name', 'ave_salary', 'sick_days']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+ 
+    writer.writeheader()
+    writer.writerows([{'sick_days': '3', 'ave_salary': '239765', 'Last_name': 'Alexeev'},
+                      {'sick_days': '1', 'ave_salary': '4345223', 'Last_name': 'Sidorenko'}])
+ 
+print("writing complete")
