34b19c838a1ad6ba4d1731cb04cfe18bdc2510d0
Жирова Анна Андреевна <AAZHirova@kkck.loc>
2020-10-21 11:29:18 +0700

    лабораторная 2 завершена

--- 2lab.py
+++ 2lab.py
@@ -6,12 +6,12 @@ class Deposit:
         self.interest_rate = interest_rate  #процентная ставка
         self.min_sum = min_sum              #минимальная сумма вклада
         self.max_sum = max_sum              #максим. сумма вклада
-        self.lasting = lasting              #длительность вклада в месяцах
+        self.lasting = lasting              #мин.длительность вклада в месяцах
 
-deposit1 = Deposit('Сбербанк', 0.06, 10000, 2000000, 24)
-deposit2 = Deposit('Сбербанк', 0.07, 100000, 3000000, 12)
-deposit3 = Deposit('Открытие', 0.07, 10000, 1000000, 24)
-deposit4 = Deposit('ВТБ', 0.05, 1000, 2000000, 24)
+deposit1 = Deposit('Сбербанк', '6%', 10000, 2000000, 12)
+deposit2 = Deposit('Альфа', '7%', 100000, 3000000, 12)
+deposit3 = Deposit('Открытие', '7%', 10000, 2000000, 24)
+deposit4 = Deposit('ВТБ', '5%', 1000, 2000000, 24)
 
 print(deposit1.__dict__, deposit2.__dict__, deposit3.__dict__, deposit4.__dict__)
 
@@ -21,6 +21,32 @@ class Сlient:
         self.deposit_amount = deposit_amount  #желаемая сумма вклада
         self.lasting = lasting                #желаемая длительность вклада в месяцах
 
-print('Введите Ваше имя, желаемую сумма вклада, желаемую длительность вклада в месяцах:')
+def change_lasting():                         #метод класса смена желаемой длительности вклада
+    print('Введите другую длительность вклада в месяцах:')
+    client.deposit_amount = int(input())
 
-client = Сlient(name=input(), deposit_amount=input(), lasting=input())
+print('Введите Ваше имя и фамилию, желаемую сумма вклада, желаемую длительность вклада в месяцах:')
+
+client = Сlient(name=input(), deposit_amount=int(input()), lasting=int(input()))
+
+answer = 'y'
+
+while answer != 'n':
+    if (client.deposit_amount >= 1000 and client.deposit_amount <= 2000000 and client.lasting >= 24):
+        print('Вам подходит вклад в банке Открытие, процентная ставка:', deposit3.interest_rate, 'и в банке банке ВТБ, процентная ставка:', deposit4.interest_rate)
+    elif (client.deposit_amount >= 1000 and client.deposit_amount <= 10000 and client.lasting >= 24):
+        print('Вам подходит вклад в банке ВТБ процентная ставка:', deposit4.interest_rate)
+    elif (client.deposit_amount >= 100000 and client.deposit_amount <= 3000000 and client.lasting >= 12):
+        print('Вам подходит вклад в банке Альфа, процентная ставка:', deposit2.interest_rate)
+    elif (client.deposit_amount >= 10000 and client.deposit_amount <= 2000000 and client.lasting >= 12):
+        print('Вам подходит вклад в банке Сбербанк, процентная ставка:', deposit1.interest_rate)
+    else:
+        print ('Подходящих банков не найдено')
+
+    print('Желаете изменить длительность вклада? y/n')
+    answer = input()
+    if answer == 'y':
+        change_lasting()        #вызов метода класса
+
+
+    
